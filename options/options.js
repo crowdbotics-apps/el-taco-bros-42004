a0c67d601df047a5db9157d9618487f89271b7d3
Crowdbotics <team@crowdbotics.com>
2023-07-08 17:53:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "el-taco-bros-42004",
+  url: "https://el-taco-bros-42004.botics.co",
+  api: "https://el-taco-bros-42004.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
